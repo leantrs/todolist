c8d050a4eb09d4f45a16228722351bb5b2340a12
Marcel Leandro <leantrs@gmail.com>
2022-07-17 18:00:27 -0700

    teste react 1

---
+++ src/redux/counterRedux/counter.reducer.js
@@ -0,0 +1,29 @@
+import {INCREASE_COUNTER, DECREASE_COUNTER} from './counter.actionTypes'
+
+
+export const COUNTER_KEY   = 'counterStore'
+
+let initialState = {
+    count: 0
+}
+
+let counterReducer = (state = initialState, action) =>{
+    // eslint-disable-next-line
+    let {type, payload} = action
+
+    switch(type){
+        case INCREASE_COUNTER:
+            return{
+                count: state.count + 1
+            }
+
+            case DECREASE_COUNTER:
+                return{
+                    count: (state.count - 1 > 0)? state.count - 1 : 1
+                }
+                default: return state
+    }
+
+}
+
+export {counterReducer}
